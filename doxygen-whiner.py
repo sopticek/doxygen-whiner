d5c475ccab64c451ed2c95eb7d713c0edc8ecc57
Daniela Duricekova <daniela.duricekova@gmail.com>
2014-05-09 13:17:18 +0200

    Added reading and parsing of warnings to doxygen-whiner.py.

--- doxygen-whiner.py
+++ doxygen-whiner.py
@@ -12,12 +12,24 @@ import sys
 
 from doxygen_whiner.args import parse as parse_args
 from doxygen_whiner.config import parse as parse_config
+from doxygen_whiner.io import read_file
+from doxygen_whiner.io import read_stdin
+from doxygen_whiner.warning import parse_warnings
 
 
 def main(argc, argv):
     args = parse_args(argv)
     config = parse_config("config.ini", "config.local.ini")
 
+    if args.file:
+        text = read_file(args.file)
+    else:
+        text = read_stdin()
+
+    warnings = parse_warnings(text)
+    # TODO: remove
+    from pprint import pprint
+    pprint(warnings)
 
 if __name__ == '__main__':
     sys.exit(main(len(sys.argv), sys.argv))

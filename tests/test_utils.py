2265e7390fa685c2d0022b2745c5d081e8cdc7c7
Daniela Ďuričeková <daniela.duricekova@protonmail.com>
2024-01-31 14:56:37 +0100

    Use different email address.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # vim:fileencoding=utf8
 #
-# Author:   Daniela Ďuričeková, daniela.duricekova@gmail.com
+# Author:   Daniela Ďuričeková, daniela.duricekova@protonmail.com
 # Date:     2014-05-09
 #
 
